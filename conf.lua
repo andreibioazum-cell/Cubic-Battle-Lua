2bbe5e8cfe99edfcf7d7c345a1119801308a0687
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-14 18:22:59 +0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,11 +1,18 @@
 function love.conf(t)
-    t.identity = "myfirstgame"
+    t.identity = "cubicbattle"
     t.version = "11.5"
-    t.window.title = "My First Game"
+    t.window.title = "Cubic Battle"
     t.window.width = 800
     t.window.height = 600
     t.window.resizable = true
-    t.window.fullscreen = false
+    t.window.fullscreen = true
+    t.window.borderless = true
+    t.window.vsync = 0          -- ОТКЛЮЧАЕМ vsync (FPS не ограничен)
     t.window.orientation = "portrait"
     t.modules.physics = false
+    t.modules.video = false      -- отключаем неиспользуемые модули
+    t.modules.joystick = false
+    t.modules.thread = false
+    t.modules.audio = false
+    t.modules.sound = false
 end
