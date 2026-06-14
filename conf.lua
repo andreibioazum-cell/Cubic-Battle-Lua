ffeef8e8a27bde914c5f3714aefdb94bf62b8ef0
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-14 22:12:13 +0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -2,15 +2,13 @@ function love.conf(t)
     t.identity = "cubicbattle"
     t.version = "11.5"
     t.window.title = "Cubic Battle"
-    t.window.width = 800
-    t.window.height = 600
-    t.window.resizable = true
     t.window.fullscreen = true
     t.window.borderless = true
-    t.window.vsync = 0          -- ОТКЛЮЧАЕМ vsync (FPS не ограничен)
-    t.window.orientation = "portrait"
+    t.window.vsync = 1           -- vsync вкл = реальный FPS экрана (60)
+    t.window.msaa = 4            -- сглаживание (анти-пиксели)
+    t.window.resizable = true
     t.modules.physics = false
-    t.modules.video = false      -- отключаем неиспользуемые модули
+    t.modules.video = false
     t.modules.joystick = false
     t.modules.thread = false
     t.modules.audio = false
