5b054f58cd7cf233dea1122fcef83f7f7fefca72
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-19 19:59:10 +0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,12 +1,11 @@
 function love.conf(t)
     t.identity = "CubicBattle3"
-    t.version = "12.0"
+    t.version = "11.5"
     t.window.title = "Cubic Battle 3"
-    t.window.fullscreen = true
-    t.window.borderless = true
+    t.window.fullscreen = false
+    t.window.resizable = true
     t.window.vsync = 1
     t.window.msaa = 4
-    t.window.resizable = true
     t.modules.physics = false
     t.modules.video = false
 end
