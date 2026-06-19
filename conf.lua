d814beb548f12e67eaaa4976ffdacf2ea62bf9ff
ehekufa <newcatroidfive5@gmail.com>
2026-06-19 15:53:29 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
-    t.identity = "cubicbattle"
+    t.identity = "com.CB3"
     t.version = "12.0"
-    t.window.title = "Cubic Battle"
+    t.window.title = "Cubic Battle 3"
     t.window.fullscreen = true
     t.window.borderless = true
     t.window.vsync = 1
